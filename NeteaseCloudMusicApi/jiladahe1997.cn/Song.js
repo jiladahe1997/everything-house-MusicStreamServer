7211309c407ebb41cd7654f406d6b4d3fd0227aa
Ren Mingrui <972931182@qq.com>
2020-04-01 22:38:31 +0800

    增加通知obs服务器功能

--- NeteaseCloudMusicApi/jiladahe1997.cn/Song.js
+++ NeteaseCloudMusicApi/jiladahe1997.cn/Song.js
@@ -5,27 +5,47 @@ const fs = require('fs')
 const fsPromises = fs.promises;
 
 async function downloadMp3(query, Request) {
+  // 通知切歌服务器
   const searchRes = await search(query, Request)
   const urlRes = await song_url({...query, id: searchRes.body.result.songs[0].id}, Request)
   const rawFile = await axios.get(urlRes.body.data[0].url, {
     responseType: 'arraybuffer'
   })
-  await fsPromises.writeFile(`./${searchRes.body.result.songs[0].name}.mp3`, rawFile.data)
+  await fsPromises.writeFile(`C:/music/${searchRes.body.result.songs[0].name}.mp3`, rawFile.data)
   console.log(`${searchRes.body.result.songs[0].name}下载完毕`);
-  sendMQ()
+  sendMQ(searchRes.body.result.songs[0].name)
 }
 
-function sendMQ(){
-  
+async function sendMQ(songName){
+  const res = await axios.post('localhost:8080/music/finishDownLoad', {
+    songName
+  })
 }
+
 module.exports = async (query, Request) => {
-  downloadMp3(query, Request)
-  return {
-    status: 200,
-    body: {
-      code: 0,
-      msg: '',
-      data: null
+  const searchRes = await search(query, Request)
+  const res = await axios.post('localhost:8080/music/orderSong', {
+    songName: searchRes.body.result.songs[0].name
+  })
+
+  if(res.msg === 'not found') {
+    downloadMp3(query, Request)
+    return {
+      status: 200,
+      body: {
+        code: 0,
+        msg: '点歌成功',
+        data: null
+      }
+    }
+  } else {
+    return {
+      status: 200,
+      body: {
+        code: 0,
+        msg: res.msg,
+        data: null
+      }
     }
   }
 }
